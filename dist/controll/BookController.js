5571250e46eb085daa81904b6088163bcc926f7d
Diogo Abreu <abreudiogo177@gmail.com>
2024-12-07 10:43:04 -0300

    Removendo node_modules e dist

--- dist/controll/BookController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/*import Database from "../db/Database";
-import Book from "../model/Book";
-
-
-export default class BookController {
-
-}
-*/ 
--- dist/model/IdentifiyEntities.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Entities = void 0;
-class Entities {
-    constructor() {
-        this.id = Math.random().toString(36).slice(2, 11);
-    }
-}
-exports.Entities = Entities;
